d976474ce9b2196b3e7e5b302478512b8bae9da8
sbahari <sbahari1090@gmail.com>
2026-01-19 08:48:32 +0700

    fix: set correct base path to /temanbudidaya/

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     // Base path for GitHub Pages
-    // Based on your repo name: sbahari.github.io
-    base: '/sbahari.github.io/temanbudidaya/',
+    base: '/temanbudidaya/',
 })
